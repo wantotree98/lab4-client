90e947648f8bdf9fe6585476f35ff417b4c80625
wantotree98 <iddota2toppo@gmail.com>
2020-11-10 18:28:38 +0800

    commit lab4

---
+++ 4.1.client.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>  //inet_addr
+#include<string.h>
+#include<unistd.h>
+
+
+int main(int argc, char*argv[]){
+
+int socket_desc;
+char* server_reply;
+struct sockaddr_in server;
+char*message;
+
+
+//create socket
+socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+if(socket_desc == -1){
+printf("Socket could not be created");
+
+}
+
+server.sin_addr.s_addr = inet_addr("192.168.10.10"); //server ip address
+server.sin_family = AF_INET;
+server.sin_port = htons (22);
+
+//connect to remote server
+if(connect(socket_desc, (struct sockaddr*)&server, sizeof(server))<0){
+
+puts("connect error");
+return 1;
+}
+
+puts("Connected!\n");
+//send some data
+
+message = "Connect";
+if(send(socket_desc, message, strlen(message), 0)<0){
+
+puts("Send Failed");
+return 1;
+}
+puts("Data send \n");
+//receive a reply from the server
+
+if(recv(socket_desc, server_reply,2000,0)<0){
+puts("Receive Failed \n");
+
+return 1;
+
+}
+close(socket_desc);
+puts("Data received \n");
+
+
+return 0;
+}
